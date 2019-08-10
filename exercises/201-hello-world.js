982f9ffd518cef381fac413832d89fbc08a2a8c9
nbal1618 <47106832+nbal1618@users.noreply.github.com>
2019-08-10 14:47:15 +0100

    Update hello function 10/8
    Is that correct ?

--- exercises/201-hello-world.js
+++ exercises/201-hello-world.js
@@ -9,3 +9,7 @@
 // Write a function "helloDefault" such that if no name is given it will return
 // 'Hello, world!'
 // Otherwise it behaves the same as the "hello" function.
+
+function hello (name) {
+  alert ("Hello, " + name);
+}
